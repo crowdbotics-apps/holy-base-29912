4aebc62fc969f7ab19efa9c509d445499b1f2914
Crowdbotics <team@crowdbotics.com>
2021-08-18 14:58:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "holy_base_29912Identifier",
+  url: "https://holy_base_29912Identifier.botics.co",
+  api: "https://holy_base_29912Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
